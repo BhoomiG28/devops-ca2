68500e2af768835947a1de4d516dae152106048b
BhoomiG28 <bhoomigarg52@gmail.com>
2026-04-17 14:58:51 +0530

    cicd running for CA2

---
+++ src/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+  res.send('Hello, this is testing of CI/CD 🚀');
+});
+
+app.get('/health', (req, res) => {
+  res.status(200).json({ status: 'UP', message: 'Server is healthy' });
+});
+
+module.exports = app;
